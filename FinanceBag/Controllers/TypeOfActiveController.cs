3704b6c3904c32bdd3fed5b1e40d70e23d9065ec
Денис Жигалов <dzhig001@gmail.com>
2022-08-30 15:13:44 +0500

    -Создан контроллер TypeOFActive. -Созданые его представления Index и Create.

---
+++ FinanceBag/Controllers/TypeOfActiveController.cs
@@ -0,0 +1,29 @@
+﻿
+using FinanceBag.Data;
+using FinanceBag.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceBag.Controllers
+{
+    public class TypeOfActiveController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TypeOfActiveController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<TypeOfActive> objTypeOfActiv = await _db.TypeOfActives.ToListAsync(); 
+            return   View(objTypeOfActiv);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
