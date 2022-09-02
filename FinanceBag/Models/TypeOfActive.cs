d53b89ded2c191042b9f58b40f34073bc39c610f
Денис Жигалов <dzhig001@gmail.com>
2022-09-02 11:01:11 +0500

    Revert "Откатить"
    This reverts commit 15a2cb5e4f715b433c9ebac977fe07b04f3d2a68.

--- FinanceBag/Models/TypeOfActive.cs
+++ FinanceBag/Models/TypeOfActive.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace FinanceBag.Models
 {
+    [Index(nameof(TypeOfActive.Type), IsUnique = true, Name = "Type_Index")]
     public class TypeOfActive
     {
         [Key]
