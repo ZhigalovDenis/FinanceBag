124aabf92a637ec89b77d5dcc1e25635ec7a9960
Денис Жигалов <dzhig001@gmail.com>
2022-09-06 10:52:17 +0500

    Реализация паттерна Репозиторий. - Добавлен базовый интерфейс репозитория. - Создан репозиторий для TypeOfActive. - Внедрена зависимость в сервисы.

---
+++ FinanceBag/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceBag.Repositories
+{
+    public interface IRepository<T1, T2> where T1: class
+    {
+      
+        Task<IEnumerable<T1>> GetAll();
+        Task<T1> GetById(T2 id);
+        Task<T1> Insert(T1 entity);
+        Task Delete(T2 id);
+        Task Save();
+    }
+}
+
---
+++ FinanceBag/Repositories/TypeOfActiveRepository.cs
@@ -0,0 +1,44 @@
+﻿using FinanceBag.Data;
+using FinanceBag.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceBag.Repositories
+{
+    public class TypeOfActiveRepository : IRepository<TypeOfActive, int>
+    {
+        private readonly ApplicationDbContext _db;
+        public TypeOfActiveRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Delete(int id)
+        {
+            var TypeOfAtciveFromDb = await _db.TypeOfActives.FindAsync(id);
+            if (TypeOfAtciveFromDb != null)
+            {
+                _db.Remove(TypeOfAtciveFromDb);
+            }
+        }
+
+        public async Task<IEnumerable<TypeOfActive>> GetAll()
+        {
+            return await _db.TypeOfActives.ToListAsync();
+        }
+
+        public async Task<TypeOfActive> GetById(int id)
+        {
+            return await _db.TypeOfActives.FindAsync(id);
+        }
+
+        public async Task<TypeOfActive> Insert(TypeOfActive entity)
+        {
+            await _db.TypeOfActives.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
