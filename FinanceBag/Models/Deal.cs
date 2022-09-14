cba1b09b20be4b7ff44af57e16400c8ab505f7e4
Денис Жигалов <dzhig001@gmail.com>
2022-09-14 15:59:58 +0500

    Добавлена датааннотация для модели Deal

--- FinanceBag/Models/Deal.cs
+++ FinanceBag/Models/Deal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,12 +13,26 @@ namespace FinanceBag.Models
     {
         [Key]
         public int Deal_id { get; set; }
+
+        [Required(ErrorMessage = "Поле <Дата/Время> пустое, заполните!!!")]
+        [DisplayName("Дата/Время")]
         public DateTime DT { get; set; }
+
+        [Required(ErrorMessage = "Поле <Кол-во> пустое, заполните!!!")]
+        [DisplayName("Кол-во")]
+        [Range(1,Int32.MaxValue, ErrorMessage = "Недопустимое значение")]
         public int Count { get; set; }
+
+        [Required(ErrorMessage = "Поле <Цена> пустое, заполните!!!")]
+        [DisplayName("Цена")]
+        [Range(0.00000001, double.MaxValue, ErrorMessage = "Недопустимое значение")]
         public decimal Price { get; set; }
         public decimal Sum { get; set; }
 
+        [Required(ErrorMessage = "Поле <_ISIN_> пустое, заполните!!!")]
+        [DisplayName("ISIN")]
         public string ISIN_id { get; set; }
+
         [ForeignKey("ISIN_id")]
 
         public Active Actives { get; set; }
