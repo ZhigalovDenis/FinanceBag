ba6cc924121c77c6ad300e4a59282a2f575d33a9
Денис Жигалов <dzhig001@gmail.com>
2022-09-23 21:29:49 +0500

    Рефакторинг.

--- FinanceBag/Controllers/HomeController.cs
+++ FinanceBag/Controllers/HomeController.cs
@@ -2,30 +2,78 @@
 using FinanceBag.Repositories;
 using FinanceBag.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging.Signing;
 using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace FinanceBag.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IRepositoryAll<Deal> _allReposytory;
+        
+
+        private readonly IRepository<TypeOfActive, int> _typeOfActiveRepository;
+        private readonly IRepository<Deal, int> _dealRepository;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IRepositoryAll<Deal> allReposytory, ILogger<HomeController> logger)
+        public HomeController(IRepository<Deal, int> dealRepository, IRepository<TypeOfActive, int> typeOfActiveRepository, ILogger<HomeController> logger)
         {
-            _allReposytory = allReposytory;
             _logger = logger;
+            _dealRepository = dealRepository;   
+            _typeOfActiveRepository = typeOfActiveRepository;
         }
 
-       
+
         public async Task<IActionResult> Index()
         {
-            AllTables objAll = new AllTables()
+            IEnumerable<TypeOfActive> objTypeOfActiv = await _typeOfActiveRepository.GetAll();
+
+            var dic  = new Dictionary<int, string>();    
+            foreach (var item in objTypeOfActiv)
             {
-                All = (List<Deal>)await _allReposytory.GetAll()
+                dic.Add(item.TypeOfActive_id, item.Type);
             }
-            return View(objAll);
-        }
+
+             IEnumerable<Deal> objDeal = await _dealRepository.GetAll();
+
+
+            
+            var objFiltered  = objDeal.GroupBy(g => g.ISIN_id).Select(s => new
+            {
+                ISIN = s.Key,
+                Type = s.Select(x => x.Actives.TypeOfActive_id).First(),
+                Tiker = s.Select(x => x.Actives.Ticker).First(),
+                Count = s.Sum(x => x.Count),
+                Sum = s.Sum(x => x.Sum),
+                Avg = s.Sum(x => x.Sum) / s.Sum(x => x.Count)
+            }).OrderBy(x => x.Tiker).ToList();
+
+
+
+
+            List<string> Ticker = new List<string>();
+            List<string> ISIN = new List<string>();
+            List<int> Count = new List<int>();
+            List<string> Type = new List<string>();
+            List<decimal> Sum = new List<decimal>();
+            List<decimal> Avg = new List<decimal>();
+
+           var val =  dic[1];
+
+            foreach (var item in objFiltered)
+            {
+                Type.Add(dic[item.Type]);
+                Ticker.Add(item.Tiker);
+                ISIN.Add(item.ISIN);
+                Count.Add(item.Count);
+                Sum.Add(item.Sum);
+                Avg.Add(item.Avg);
+            }
+
+
+            return View(objFiltered);
+
 
 
 
--- FinanceBag/ViewModel/AllTables.cs
+++ FinanceBag/ViewModel/AllTables.cs
@@ -6,5 +6,6 @@ namespace FinanceBag.ViewModel
     public class AllTables
     {
         public List<Deal> All { get; set; }
+        public List<string> MyProperty { get; set; }
     }
 }
