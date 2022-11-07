b6d1c7c7313c6886483025798f46b824f77983da
Денис Жигалов <dzhig001@gmail.com>
2022-11-07 14:21:10 +0500

    Метод CalculateProfit в классе CalculateService обернут в Task.Run

--- FinanceBag/Services/CalculateService.cs
+++ FinanceBag/Services/CalculateService.cs
@@ -6,26 +6,27 @@ namespace FinanceBag.Services
     {
         public async Task<AnaliticsViewModel> CalculateProfit(AnaliticsViewModel model)
         {
-           
-            List<decimal> ProfitOfActive = new List<decimal>();
-            List<decimal> ProfitOfAllActive = new List<decimal>();
+           return await Task.Run(() =>
+           { 
+                List<decimal> ProfitOfActive = new List<decimal>();
+                List<decimal> ProfitOfAllActive = new List<decimal>();
 
-            for (int i = 0; i < model.vM_Count.Count; i++)
-            {
-                decimal profitOfActive =   model.vM_CurrentPrice[i] - model.vM_Avg[i];
-                decimal profitOfAllActive = (model.vM_Count[i] * model.vM_CurrentPrice[i]) - model.vM_Sum[i];
-                ProfitOfActive.Add(profitOfActive);
-                ProfitOfAllActive.Add(profitOfAllActive);
-            }
-            model.vM_ProfitOfActive = ProfitOfActive;
-            model.vM_ProfitOfAllActive =  ProfitOfAllActive;
+                for (int i = 0; i < model.vM_Count.Count; i++)
+                {
+                    decimal profitOfActive =   model.vM_CurrentPrice[i] - model.vM_Avg[i];
+                    decimal profitOfAllActive = (model.vM_Count[i] * model.vM_CurrentPrice[i]) - model.vM_Sum[i];
+                    ProfitOfActive.Add(profitOfActive);
+                    ProfitOfAllActive.Add(profitOfAllActive);
+                }
+                model.vM_ProfitOfActive = ProfitOfActive;
+                model.vM_ProfitOfAllActive =  ProfitOfAllActive;
 
-            model.vM_TotalCosts = model.vM_Sum.Sum();
-            model.vM_ProfitValue = Math.Round(model.vM_ProfitOfAllActive.Sum(),2);
+                model.vM_TotalCosts = model.vM_Sum.Sum();
+                model.vM_ProfitValue = Math.Round(model.vM_ProfitOfAllActive.Sum(),2);
 
-            //Обернуть метод в таску
+                return  model;
 
-            return  model;
+            });
         }
 
 
